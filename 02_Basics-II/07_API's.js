a5c28f9a0d8e0f8ca1e30f02936c2efa99db0728
Siva Jallipalli <kumar.jallipalli@gmail.com>
2024-04-19 07:07:51 +0530

    API & JSON

---
+++ 02_Basics-II/07_API's.js
@@ -0,0 +1,36 @@
+/**
+ *  API:
+ *      - API - application programming interface
+ *      - An API is a set of rules/protocols that enables software applications to communicate with each other to exchange data, features and functionality.
+ *      - APIs simplify and accelerate the software development by allowing developers to integrate data, services and capabilities from other applications, instead of developing them from scratch.
+ *      - APIs helps in abstarction which allows sharing of only the required information, while keeping other internal system details hidden, 
+ *              - This helps with system security. Servers or devices do not have to fully expose data
+ * 
+ *      - previously, the data to be exchanged is in the form of "XML" [ which is a bit complex ]
+ *      - Now, that data is exchanged in the form of "JSON"
+*/
+
+
+
+/**
+ *  JSON:
+ *      - JSON is plain text written in JavaScript object notation
+ *      - JSON is a lightweight data-interchange format
+ *      - JSON format is syntactically similar to the code for creating JavaScript objects. [ keys should be in string format only]
+ *      - Since the format is text only, JSON data can easily be sent between computers, and used by any programming language.
+ *      - JSON format - '{"name":"John", "age":30, "car":null}'
+*/
+
+
+
+/**
+ *  How to make a JSON from JS objects..?
+ *          1. NO variable, only { }
+ *          2. write down the properties just like an object 
+ *          3. make all the keys in "string" format
+*/
+
+/**
+ * JavaScript has a built in function for converting JSON strings into JavaScript objects:  - JSON.parse()
+ * JavaScript also has a built in function for converting an object into a JSON string:     - JSON.stringify()
+*/
